621188ecfbfa4daf40e567b5b27289cd7a7061eb
Vladislav Malykhin <vl.malykhin2211@yahoo.com>
2023-11-05 13:57:18 +0300

    first commit

---
+++ task_2.py
@@ -0,0 +1,7 @@
+stage_1 = 'Australopithecus'
+stage_2 = 'Homo Habilis'
+stage_3 = 'Homo Erectus'
+stage_4 = 'Homo Neanderthalensis'
+stage_5 = 'Homo Sapiens Sapiens'
+print(stage_1, stage_2, stage_3, stage_4, stage_5,  sep=' => ')
+
