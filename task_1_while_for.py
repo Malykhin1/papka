6a4ce05aa8b47a9997fc41563ab94ca371a5d70b
Vladislav Malykhin <vl.malykhin2211@yahoo.com>
2023-11-05 22:41:44 +0300

    modified:   task_1_while_for.py

--- task_1_while_for.py
+++ task_1_while_for.py
@@ -1,1 +1,9 @@
-n = int(input())
+n = int(input("Введите количество чисел: "))
+numbers = []
+
+for i in range(n):
+    number = int(input("Введите число: "))
+    numbers.append(number)
+
+count_zero = numbers.count(0)
+print("Количество нулей:", count_zero)
