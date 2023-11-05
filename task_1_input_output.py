88d6d35f2d3c915115221ad78f36ff839a604069
Vladislav Malykhin <vl.malykhin2211@yahoo.com>
2023-11-05 16:21:41 +0300

    renamed:    task_3.py -> task_1_float_int.py renamed:    task_1.py -> task_1_input_output.py renamed:    task_4.py -> task_2_float_int.py renamed:    task_2.py -> task_2_input_output.py

--- task_1_input_output.py
+++ task_1_input_output.py
@@ -1,2 +1,3 @@
 print(input('Это желторотый питон по кличке "Каа". Возраст: 34 года.'))
+
 #ссылка на репозиторий в GitHub: https://github.com/Malykhin1/python_tasks
--- task_2_input_output.py
+++ task_2_input_output.py
@@ -4,5 +4,6 @@ stage_3 = 'Homo Erectus'
 stage_4 = 'Homo Neanderthalensis'
 stage_5 = 'Homo Sapiens Sapiens'
 print(stage_1, stage_2, stage_3, stage_4, stage_5,  sep=' => ')
+
 #ссылка на репозиторий в GitHub: https://github.com/Malykhin1/python_tasks
 
