aa93339cd893885530856aecf9baef98beea7a9d
Vladislav Malykhin <vl.malykhin2211@yahoo.com>
2023-11-05 15:21:02 +0300

    new file:   task_3.py

---
+++ task_3.py
@@ -0,0 +1,9 @@
+print('Введите ширину прямоугольника:')
+a = float(input())
+print('Введите длинну прямоугольника:')
+b = float(input())
+perimetr = int(a*2 + b*2)
+square = int(a*b)
+print(f'Периметр равен: {perimetr}\nПлощадь равна: {square}')
+
+#ссылка на репозиторий в GitHub: https://github.com/Malykhin1/python_tasks
