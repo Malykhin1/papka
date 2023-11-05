94f68522aebf79fcfb2f90394603a7d20f83fb8d
Vladislav Malykhin <vl.malykhin2211@yahoo.com>
2023-11-05 16:10:41 +0300

    modified:   task_4.py

--- task_4.py
+++ task_4.py
@@ -1,9 +1,9 @@
 number = int(input("Введите пятизначное целое число: "))
 units = number % 10
-dozens = number % 100 // 10
+tens = number % 100 // 10
 hundreds = number % 1000 // 100
 thousands = number % 10000 // 1000
-doz_of_thous = number // 10000
-print((dozens ** units) * hundreds / (doz_of_thous - thousands))
+tens_of_thous = number // 10000
+print((tens ** units) * hundreds / (tens_of_thous - thousands))
 
 #ссылка на репозиторий в GitHub: https://github.com/Malykhin1/python_tasks
