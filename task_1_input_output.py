849477a26bde1acfaf4bbfc6ab792f611a8e39e1
Vladislav Malykhin <vl.malykhin2211@yahoo.com>
2023-11-06 11:38:06 +0300

    modified:   task_1_input_output.py

--- task_1_input_output.py
+++ task_1_input_output.py
@@ -1,3 +1,7 @@
-print(input('Это желторотый питон по кличке "Каа". Возраст: 34 года.'))
+name_pet = input('Введите имя питомца: ')
+age_pet = input('Введите возраст питомца: ')
+type_pet = input('Введите тип питомца: ')
+
+print('У вас', type_pet+', ''зовут',name_pet+'.','Возраст:', age_pet,'года.')
 
 #ссылка на репозиторий в GitHub: https://github.com/Malykhin1/python_tasks
